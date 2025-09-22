25d4a55291bbf98928e3783d2f79bfc98dd67b50
Gal Cabrales <cabralespriegogalilea@gmail.com>
2025-09-22 14:50:11 -0600

    Crea funciones vacias

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaDeAmigos = [];
+
+function agregarAmigos() {
+
+}
+
+function actualizarListaAmigos() {
+
+}
+
+function sortearAmigos() {
+    
+}
